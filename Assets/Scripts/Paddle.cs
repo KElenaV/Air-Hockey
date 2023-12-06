9e4e3374fc1ecac087ac9d17255e42fdac9aa979
KElenaV <81685064+KElenaV@users.noreply.github.com>
2023-12-06 10:40:35 -0700

    Paddle Input

---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Paddle : MonoBehaviour
+{
+    [SerializeField] private float _speed = 10;
+
+    private Rigidbody _rigidbody;
+    private Vector2 _direction;
+
+    private void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        _rigidbody.velocity = new Vector3(_direction.x, 0, _direction.y) * _speed;
+    }
+
+    public void OnMove(InputAction.CallbackContext context) 
+        => _direction = context.ReadValue<Vector2>();
+}
