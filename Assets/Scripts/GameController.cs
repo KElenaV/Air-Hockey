6f20ee30d377e1280491b3e78552c6a38be2fcc1
KElenaV <81685064+KElenaV@users.noreply.github.com>
2023-12-06 21:14:47 -0700

    End game

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,5 +1,7 @@
+using System.Collections;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour
 {
@@ -8,12 +10,20 @@ public class GameController : MonoBehaviour
 
     private int _firstPlayerScore;
     private int _secondPlayerScore;
+    private bool _isGameOver = false;
+    private float _restartDelay = 3f;
 
     private void OnEnable() 
         => _puck.InGate += OnInGate;
 
     private void OnDisable() 
         => _puck.InGate -= OnInGate;
+
+    private void Update()
+    {
+        if (_isGameOver)
+            StartCoroutine(RestartGame());
+    }
 
     private void OnInGate(float gatePositionX)
     {
@@ -23,5 +33,23 @@ public class GameController : MonoBehaviour
             _secondPlayerScore++;
 
         _score.text = $"{_firstPlayerScore} : {_secondPlayerScore}";
+
+        if (_firstPlayerScore == 3)
+            ShowWinner(1);
+        if (_secondPlayerScore == 3)
+            ShowWinner(2);
+    }
+
+    private void ShowWinner(int winPlayerNumber)
+    {
+        _score.text = $"Player {winPlayerNumber} win!";
+        _puck.gameObject.SetActive(false);
+        _isGameOver = true;
+    }
+
+    private IEnumerator RestartGame()
+    {
+        yield return new WaitForSeconds(_restartDelay);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
